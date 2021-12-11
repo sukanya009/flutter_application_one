b0a4fb34c2ed83a7dd5f7d02da3e3ad6057f20ea
mayur <mayurrathor007@gmail.com>
2021-12-11 18:15:54 +0530

    new Project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(OneApp());
+}
+class OneApp extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Container(
+        child: Text('welcome to 30days flutter'),
+      ),
+    );
+      
+    
+  }
+}
